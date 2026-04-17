74943a54114f0195370367ef80dc98a30e160a7f
faress1212 <fares.shawat2@gmail.com>
2026-04-17 23:12:42 +0200

    Client Server Files Add

--- client.py
+++ client.py
@@ -3,7 +3,14 @@ import socket
 from threading import Thread
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("mainline.proxy.rlwy.net", 8080))
+
+# منع الكونكشن من الانقطاع
+client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 30)   # ابدأ بعد 30 ثانية
+client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 10)  # كل 10 ثواني
+client_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5)     # 5 محاولات
+
+client_socket.connect(("mainline.proxy.rlwy.net", 45447))
 
 home = CTk(fg_color="#7d2252")
 home.geometry('300x400')
@@ -14,18 +21,29 @@ chatarea.pack(pady=10)
 entry = CTkEntry(home, width=280)
 entry.pack(pady=5)
 
-def sendmsg():
+connected = True  # متغير لحالة الكونكشن
+
+def sendmsg(event=None):
+    if not connected:
+        chatarea.insert("end", "⚠ انت مش متصل!\n")
+        return
     msg = entry.get()
     if msg:
-        client_socket.sendall(msg.encode())
-        chatarea.insert("end", "Me: " + msg + "\n")
-        chatarea.see("end")
-        entry.delete(0, "end")
+        try:
+            client_socket.sendall(msg.encode())
+            chatarea.insert("end", "Me: " + msg + "\n")
+            chatarea.see("end")
+            entry.delete(0, "end")
+        except:
+            chatarea.insert("end", "⚠ فشل الإرسال!\n")
 
 sendbtn = CTkButton(home, text="Send", command=sendmsg)
 sendbtn.pack(pady=5)
 
+entry.bind("<Return>", sendmsg)
+
 def receivemsg():
+    global connected
     while True:
         try:
             data = client_socket.recv(1024).decode()
@@ -35,6 +53,8 @@ def receivemsg():
                     chatarea.see("end")
                 ))
         except:
+            connected = False
+            home.after(0, lambda: chatarea.insert("end", "⚠ انقطع الاتصال!\n"))
             break
 
 Thread(target=receivemsg, daemon=True).start()
