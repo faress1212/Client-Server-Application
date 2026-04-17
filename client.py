8880439bcf5f6affd1b6f2f3bede2cf3c7b07056
faress1212 <fares.shawat2@gmail.com>
2026-04-17 22:23:34 +0200

    Client Server Files Add

---
+++ client.py
@@ -0,0 +1,37 @@
+from customtkinter import *
+import socket
+from threading import Thread
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(("monorail.proxy.rlwy.net", 50050))
+
+home=CTk(fg_color="#7d2252")
+home.geometry('300x400')
+
+chatarea=CTkTextbox(home,width=280,height=300)
+chatarea.pack(pady=10)
+
+entry=CTkEntry(home,width=280)
+entry.pack(pady=5)
+
+def sendmsg():
+    msg = entry.get()
+    if msg:
+        client_socket.sendall(msg.encode())
+        chatarea.insert("end", "Me: " + msg + "\n")
+        entry.delete(0, "end")
+
+sendbtn =CTkButton(home, text="Send", command=sendmsg)
+sendbtn.pack(pady=5)
+
+def receivemsg():
+    while True:
+        try:
+            data = client_socket.recv(1024).decode()
+            home.after(0, lambda: chatarea.insert("end", "Other: " + data + "\n"))
+        except:
+            break
+
+Thread(target=receivemsg, daemon=True).start()
+
+home.mainloop()
